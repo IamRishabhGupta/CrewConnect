606e41db6e7e652673e5042dd3915c2e9162529c
Rishabh Gupta <rishabh9aug@gmail.com>
2023-02-03 03:07:30 +0530

    base activity created

---
+++ app/src/main/java/com/example/crewconnect/BaseActivity.kt
@@ -0,0 +1,55 @@
+package com.example.crewconnect
+
+import android.app.Dialog
+import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+
+open class BaseActivity : AppCompatActivity() {
+
+    private var doubleBackToExitPressedOnce=false
+
+    private lateinit var mProgressDialog:Dialog
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_base)
+
+    }
+
+    fun showProgressDialog(text:String){
+        mProgressDialog= Dialog(this)
+        mProgressDialog.setContentView(R.layout.dialog_process)
+        mProgressDialog.setTitle(text)
+        mProgressDialog.show()
+    }
+    fun hideProgressDialog(){
+        mProgressDialog.dismiss()
+    }
+
+    fun getCurrentUserId():String{
+        return FirebaseAuth.getInstance().currentUser!!.uid
+    }
+
+    fun doubleBackToExit(){
+        if(doubleBackToExitPressedOnce){
+            super.onBackPressed()
+            return
+        }
+        this.doubleBackToExitPressedOnce=true
+        Toast.makeText(this, "press again to exit", Toast.LENGTH_SHORT).show()
+
+        Handler().postDelayed({doubleBackToExitPressedOnce=false},2000)
+    }
+
+    fun showErrorSnackBar(message:String){
+        val snackBar=Snackbar.make(findViewById(android.R.id.content),message,Snackbar.LENGTH_LONG)
+        val snackBarView=snackBar.view
+        snackBarView.setBackgroundColor(ContextCompat.getColor(this,R.color.snackbar_background_color))
+        snackBar.show()
+    }
+}
