1f4da063199f392fd37d5d6523f0a0e0da5c1da6
Rishabh Gupta <rishabh9aug@gmail.com>
2023-02-03 21:30:35 +0530

    craeted new pacakage and added user and firestore file

---
+++ app/src/main/java/com/example/crewconnect/firebase/FirestoreClass.kt
@@ -0,0 +1,15 @@
+package com.example.crewconnect.firebase
+
+import com.example.crewconnect.SignUpActivity
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.auth.User
+
+class FirestoreClass {
+
+    private val mFireStore=FirebaseFirestore.getInstance()
+
+    fun registerUser(activity: SignUpActivity,unserInfo:User)
+    {
+
+    }
+}
